8dc5736cfec71b3a7f38bf3def7fdde99508724c
lasithhettiarachchi <lasith123456@gmail.com>
2024-04-14 22:25:50 +0530

    first-commit

---
+++ scripts/gallery_scripts/map.js
@@ -0,0 +1,43 @@
+
+// Inserting links for each group id
+
+document.getElementById("group-1").addEventListener("click", function () {
+    document.location.href="";
+});
+
+
+document.getElementById("group-3").addEventListener("click", function () {
+    document.location.href="";
+});
+
+document.getElementById("group-4").addEventListener("click", function () {
+    document.location.href="../html/gallery.html";
+});
+
+document.getElementById("group-5").addEventListener("click", function () {
+    document.location.href="";
+});
+
+document.getElementById("group-6").addEventListener("click", function () {
+    document.location.href="";
+});
+
+document.getElementById("group-7").addEventListener("click", function () {
+    document.location.href="../html/map.html";
+});
+
+document.getElementById("group-8").addEventListener("click", function () {
+    document.location.href="";
+});
+
+document.getElementById("group-9").addEventListener("click", function () {
+    document.location.href="";
+});
+
+document.getElementById("group-10").addEventListener("click", function () {
+    document.location.href="";
+});
+
+document.getElementById("group-11").addEventListener("click", function () {
+    document.location.href="";
+});
